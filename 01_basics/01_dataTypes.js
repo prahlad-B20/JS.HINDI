cfa6242d9e32d3bd2ec5d2f4ecd48dc87a67583d
prahlad-B20 <105708646+prahlad-B20@users.noreply.github.com>
2023-12-05 11:18:05 +0000

    container config done

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code new version
+
+alert( 3 + 3)  we are using node js ,not browser
+
+console.log(3+3) // code readibility should be high 
+
+console.log("rahul")
+
+let age= 18
+let isLognIn= false
+let state;
+
+// number=> 2 to power 53
+// bigint 
+// String => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof" undefined ")
+
