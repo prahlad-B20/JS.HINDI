ffddd10833c2d4215ca4dc4e8640791894044288
prahlad-B20 <105708646+prahlad-B20@users.noreply.github.com>
2023-12-05 10:28:42 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accounId=1455
+let accountEmail="rahul@gmail.com"
+var accountPassword="12345"
+accountcity = "kanpur"
+
+accountEmail="ankit@gmail.com"
+ accountPassword="7894"
+
+console.log(accounId);
+console.table([accounId,accountEmail,accountPassword,accountcity]);
